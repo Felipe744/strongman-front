2070a7eeebeb524c30b49fb3e29a2ebcdba355d5
Felipe744 <felipe_oliveira74@hotmail.com>
2022-03-18 12:23:01 -0300

    front

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'strongman-front';
+  showFiller = false;
+
+  typesOfShoes: string[] = ['Clientes', 'Academia', 'Treino'];
 }
